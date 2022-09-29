de2f65e86eb0743acb4d19857977b3430a314f5c
Robert Title <rtitle@broadinstitute.org>
2022-09-29 13:52:22 -0400

    Fix exception status

--- service/src/main/java/bio/terra/landingzone/stairway/flight/exception/LandingZoneIdNotFound.java
+++
@@ -1,9 +0,0 @@
-package bio.terra.landingzone.stairway.flight.exception;
-
-import bio.terra.common.exception.InternalServerErrorException;
-
-public class LandingZoneIdNotFound extends InternalServerErrorException {
-  public LandingZoneIdNotFound(String message) {
-    super(message);
-  }
-}
