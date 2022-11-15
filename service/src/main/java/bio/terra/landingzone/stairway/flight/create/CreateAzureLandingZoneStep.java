914b86e7b89c158c48a76e0163e95d23d00144a9
giventocode <3589801+giventocode@users.noreply.github.com>
2022-11-15 09:41:21 -0500

    undo in create step deletes the landing zone

--- service/src/main/java/bio/terra/landingzone/stairway/flight/create/CreateAzureLandingZoneStep.java
+++ service/src/main/java/bio/terra/landingzone/stairway/flight/create/CreateAzureLandingZoneStep.java
@@ -5,6 +5,7 @@ import bio.terra.landingzone.library.LandingZoneManagerProvider;
 import bio.terra.landingzone.library.landingzones.definition.DefinitionVersion;
 import bio.terra.landingzone.library.landingzones.deployment.DeployedResource;
 import bio.terra.landingzone.library.landingzones.management.LandingZoneManager;
+import bio.terra.landingzone.library.landingzones.management.deleterules.LandingZoneRuleDeleteException;
 import bio.terra.landingzone.model.LandingZoneTarget;
 import bio.terra.landingzone.service.landingzone.azure.exception.LandingZoneDefinitionNotFound;
 import bio.terra.landingzone.service.landingzone.azure.model.DeployedLandingZone;
@@ -78,6 +79,26 @@ public class CreateAzureLandingZoneStep implements Step {
 
   @Override
   public StepResult undoStep(FlightContext context) {
+    FlightMap inputMap = context.getInputParameters();
+    FlightUtils.validateRequiredEntries(inputMap, LandingZoneFlightMapKeys.LANDING_ZONE_ID);
+    var landingZoneId = inputMap.get(LandingZoneFlightMapKeys.LANDING_ZONE_ID, UUID.class);
+
+    // Read working map parameters
+    FlightMap workingMap = context.getWorkingMap();
+    FlightUtils.validateRequiredEntries(workingMap, LandingZoneFlightMapKeys.BILLING_PROFILE);
+    var billingProfile =
+        workingMap.get(LandingZoneFlightMapKeys.BILLING_PROFILE, ProfileModel.class);
+
+    var lzManager =
+        landingZoneManagerProvider.createLandingZoneManager(
+            LandingZoneTarget.fromBillingProfile(billingProfile));
+
+    try {
+      lzManager.deleteResources(landingZoneId.toString());
+    } catch (LandingZoneRuleDeleteException e) {
+      return new StepResult(StepStatus.STEP_RESULT_FAILURE_FATAL, e);
+    }
+
     return StepResult.getStepResultSuccess();
   }
 
