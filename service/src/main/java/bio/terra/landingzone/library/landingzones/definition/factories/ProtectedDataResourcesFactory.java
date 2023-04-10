d22963fdf1c5c4cb8fef60f90943cee330a2468c
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-04-10 14:57:56 -0700

    Disable creation of dummy disk resources.

--- service/src/main/java/bio/terra/landingzone/library/landingzones/definition/factories/ProtectedDataResourcesFactory.java
+++ service/src/main/java/bio/terra/landingzone/library/landingzones/definition/factories/ProtectedDataResourcesFactory.java
@@ -1,13 +1,20 @@
 package bio.terra.landingzone.library.landingzones.definition.factories;
 
+import bio.terra.landingzone.library.landingzones.definition.ArmManagers;
 import bio.terra.landingzone.library.landingzones.definition.DefinitionHeader;
 import bio.terra.landingzone.library.landingzones.definition.DefinitionVersion;
 import bio.terra.landingzone.library.landingzones.definition.LandingZoneDefinable;
 import java.util.List;
 
 public class ProtectedDataResourcesFactory extends ArmClientsDefinitionFactory {
-  private final String LZ_NAME = "Fake Landing Zone Base Resources";
-  private final String LZ_DESC = "Every resource which Cromwell has and disk";
+  private final String LZ_NAME = "Protected Data Landing Zone";
+  private final String LZ_DESC = "Cromwell Landing Zone resources and some new resources (TBD)";
+
+  public ProtectedDataResourcesFactory() {}
+
+  public ProtectedDataResourcesFactory(ArmManagers armManagers) {
+    super(armManagers);
+  }
 
   @Override
   public DefinitionHeader header() {
