683ebaedb8bb11c7a0d779763fbdd6f103b2a954
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-04-13 19:36:33 -0700

    POC - initial code.

--- service/src/main/java/bio/terra/landingzone/stairway/flight/StepsDefinitionProvider.java
+++ service/src/main/java/bio/terra/landingzone/stairway/flight/StepsDefinitionProvider.java
@@ -1,11 +1,21 @@
 package bio.terra.landingzone.stairway.flight;
 
+import bio.terra.landingzone.library.configuration.LandingZoneAzureConfiguration;
 import bio.terra.stairway.RetryRule;
 import bio.terra.stairway.Step;
+import java.util.List;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.List;
-
 public interface StepsDefinitionProvider {
-    public List<Pair<Step, RetryRule>> get();
+  /**
+   * Returns list of required together with corresponding retry rule. Each step creates one specific
+   * landing zone resource. Order of steps matters. If a resource1 has dependency on another
+   * resource2 then resource1 should be deployed first and save its id or another required
+   * information for resource2 (using flight working map). So, step for resource1 should go first in
+   * the list definition.
+   *
+   * @param landingZoneAzureConfiguration
+   * @return List of pairs of steps and step's retry rule
+   */
+  List<Pair<Step, RetryRule>> get(LandingZoneAzureConfiguration landingZoneAzureConfiguration);
 }
