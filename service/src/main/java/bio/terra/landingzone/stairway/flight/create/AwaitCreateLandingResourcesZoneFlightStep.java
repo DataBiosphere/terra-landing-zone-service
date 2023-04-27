af91bf99e2ae71b95c6ced5015d7c065bc2ee525
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-04-27 10:52:16 -0700

    Fix sonar issues.

--- service/src/main/java/bio/terra/landingzone/stairway/flight/create/AwaitCreateLandingResourcesZoneFlightStep.java
+++ service/src/main/java/bio/terra/landingzone/stairway/flight/create/AwaitCreateLandingResourcesZoneFlightStep.java
@@ -10,8 +10,12 @@ import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
 import bio.terra.stairway.StepStatus;
 import bio.terra.stairway.exception.RetryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AwaitCreateLandingResourcesZoneFlightStep implements Step {
+  private static final Logger logger =
+      LoggerFactory.getLogger(AwaitCreateLandingResourcesZoneFlightStep.class);
   public static final int FLIGHT_POLL_SECONDS = 1;
   // successful flight takes 10 min to deploy all resources,
   // in case last step failed we need to delete all the resources
@@ -40,14 +44,22 @@ public class AwaitCreateLandingResourcesZoneFlightStep implements Step {
                           "Failed to create landing zone Azure resources.")));
     }
 
-    // we need to pass result from sub-flight
-    var deployedLandingZone =
-        subFlightState
-            .getResultMap()
-            .get()
-            .get(JobMapKeys.RESPONSE.getKeyName(), DeployedLandingZone.class);
-    context.getWorkingMap().put(JobMapKeys.RESPONSE.getKeyName(), deployedLandingZone);
-
+    if (subFlightState.getResultMap().isPresent()) {
+      // we need to pass result from sub-flight
+      var deployedLandingZone =
+          subFlightState
+              .getResultMap()
+              .get()
+              .get(JobMapKeys.RESPONSE.getKeyName(), DeployedLandingZone.class);
+      context.getWorkingMap().put(JobMapKeys.RESPONSE.getKeyName(), deployedLandingZone);
+    } else {
+      return new StepResult(
+          StepStatus.STEP_RESULT_FAILURE_FATAL,
+          new LandingZoneCreateException(
+              String.format(
+                  "Expected landing zone result from sub-flight with id='%s' not found.",
+                  subFlightId)));
+    }
     return StepResult.getStepResultSuccess();
   }
 
