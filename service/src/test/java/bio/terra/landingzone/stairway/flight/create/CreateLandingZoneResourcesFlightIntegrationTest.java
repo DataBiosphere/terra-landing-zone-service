675b884bbf114d21e96b9084c487ab6dac485037
Blake Geno <bgeno@broadinstitute.org>
2023-05-18 15:38:51 -0400

    add asserts and resource deletion

--- service/src/test/java/bio/terra/landingzone/stairway/flight/create/CreateLandingZoneResourcesFlightIntegrationTest.java
+++ service/src/test/java/bio/terra/landingzone/stairway/flight/create/CreateLandingZoneResourcesFlightIntegrationTest.java
@@ -9,9 +9,11 @@ import bio.terra.common.stairway.StairwayComponent;
 import bio.terra.landingzone.job.exception.InternalStairwayException;
 import bio.terra.landingzone.job.exception.JobNotFoundException;
 import bio.terra.landingzone.library.landingzones.LandingZoneTestFixture;
+import bio.terra.landingzone.library.landingzones.management.LandingZoneManager;
 import bio.terra.landingzone.service.landingzone.azure.LandingZoneService;
 import bio.terra.landingzone.service.landingzone.azure.model.LandingZoneRequest;
 import bio.terra.landingzone.stairway.flight.StepsDefinitionFactoryType;
+import bio.terra.landingzone.stairway.flight.create.resource.step.AggregateLandingZoneResourcesStep;
 import bio.terra.profile.model.CloudPlatform;
 import bio.terra.profile.model.ProfileModel;
 import bio.terra.stairway.FlightState;
@@ -22,9 +24,8 @@ import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,26 +34,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Tag("integration")
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest()
-
 @SpringBootApplication(
     scanBasePackages = {
-        "bio.terra.common.logging",
-        "bio.terra.common.migrate",
-        "bio.terra.common.kubernetes",
-        "bio.terra.landingzone"
+      "bio.terra.common.logging",
+      "bio.terra.common.migrate",
+      "bio.terra.common.kubernetes",
+      "bio.terra.common.stairway",
+      "bio.terra.landingzone"
     },
-    exclude = {DataSourceAutoConfiguration.class}
-    )
+    exclude = {DataSourceAutoConfiguration.class})
 @EnableRetry
 @EnableTransactionManagement
 public class CreateLandingZoneResourcesFlightIntegrationTest extends LandingZoneTestFixture {
-
 
   @Mock private BearerToken bearerToken;
 
@@ -66,9 +67,16 @@ public class CreateLandingZoneResourcesFlightIntegrationTest extends LandingZone
   UUID landingZoneId;
   ProfileModel profile;
   LandingZoneRequest request;
+  LandingZoneManager landingZoneManager;
+
+  @BeforeAll
+  static void init() {
+    Awaitility.setDefaultPollInterval(Duration.ofSeconds(5));
+  }
 
   @BeforeEach
   void setup() {
+
     jobId = UUID.randomUUID();
     landingZoneId = UUID.randomUUID();
 
@@ -82,16 +90,24 @@ public class CreateLandingZoneResourcesFlightIntegrationTest extends LandingZone
             .id(UUID.randomUUID());
     request =
         new LandingZoneRequest(
-            StepsDefinitionFactoryType.PROTECTED_DATA_DEFINITION_STEPS_PROVIDER_NAME.getValue(),
+            StepsDefinitionFactoryType.CROMWELL_BASE_DEFINITION_STEPS_PROVIDER_TYPE.getValue(),
             "v1",
             Map.of(),
             profile.getId(),
             Optional.of(landingZoneId),
             true);
+    landingZoneManager =
+        LandingZoneManager.createLandingZoneManager(
+            tokenCredential, azureProfile, resourceGroup.name());
+  }
+
+  @AfterEach
+  void cleanUp() throws Exception {
+    landingZoneManager.deleteResources(landingZoneId.toString());
   }
 
   @Test
-  void test() {
+  void createResourcesFlightDeploysCromwellResources() throws Exception {
     String resultPath = "";
     landingZoneService.startLandingZoneResourceCreationJob(
         jobId.toString(), request, profile, landingZoneId, bearerToken, resultPath);
@@ -105,11 +121,28 @@ public class CreateLandingZoneResourcesFlightIntegrationTest extends LandingZone
             });
     var flightState = retrieveFlightState(jobId.toString());
     assertThat(flightState.getFlightStatus(), oneOf(FlightStatus.READY, FlightStatus.SUCCESS));
+
+    var lzIdString = landingZoneId.toString();
+
+    var resources = landingZoneManager.reader().listSharedResources(lzIdString);
+    assertThat(resources, hasSize(AggregateLandingZoneResourcesStep.deployedResourcesKeys.size()));
+
+    landingZoneManager.deleteResources(landingZoneId.toString());
+
+    // Immediate listing after deletion may return transient resources results.
+    await()
+        .atMost(Duration.ofSeconds(120))
+        .untilAsserted(
+            () -> {
+              var landingZoneResources =
+                  landingZoneManager.reader().listAllResources(landingZoneId.toString());
+              assertThat(landingZoneResources, empty());
+            });
   }
 
-
   // just a clone of the method in LandingZoneService
-  // but using the stairwayComponent directly means we don't have to fuss around with authentication,
+  // but using the stairwayComponent directly means we don't have to fuss around with
+  // authentication,
   // which brings in the requirement for a lot more mocking
   private FlightState retrieveFlightState(String jobId) {
     try {
@@ -121,5 +154,4 @@ public class CreateLandingZoneResourcesFlightIntegrationTest extends LandingZone
       throw new InternalStairwayException(stairwayEx);
     }
   }
-
 }
