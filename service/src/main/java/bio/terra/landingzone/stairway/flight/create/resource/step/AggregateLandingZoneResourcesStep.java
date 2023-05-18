c283c09e11a8856c5d12fcb99defeed4409ec31b
Blake Geno <bgeno@broadinstitute.org>
2023-05-18 15:14:47 -0400

    change list visibility for testing purposes

--- service/src/main/java/bio/terra/landingzone/stairway/flight/create/resource/step/AggregateLandingZoneResourcesStep.java
+++ service/src/main/java/bio/terra/landingzone/stairway/flight/create/resource/step/AggregateLandingZoneResourcesStep.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 
 public class AggregateLandingZoneResourcesStep implements Step {
   // TODO: only shared resources?
-  private List<String> deployedResourcesKeys =
+  public static List<String> deployedResourcesKeys =
       List.of(
           CreateVnetStep.VNET_RESOURCE_KEY,
           CreateBatchAccountStep.BATCH_ACCOUNT_RESOURCE_KEY,
