6120c2690a780d5a44f49b0f3288fff25cc2e2cb
Blake Geno <bgeno@broadinstitute.org>
2023-05-20 15:25:51 -0400

    fix fault image tag

--- service/render-test-config.sh
+++ service/render-test-config.sh
@@ -5,7 +5,7 @@
 
 
 VAULT_TOKEN=${1:-$(cat $HOME/.vault-token)}
-DSDE_TOOLBOX_DOCKER_IMAGE=broadinstitute/dsde-toolbox:consul-0.20.0
+DSDE_TOOLBOX_DOCKER_IMAGE=broadinstitute/dsde-toolbox:dev
 VAULT_AZURE_MANAGED_APP_CLIENT_PATH=secret/dsde/terra/azure/common/managed-app-publisher
 AZURE_MANAGED_APP_CLIENT_OUTPUT_FILE_PATH="$(dirname $0)"/src/test/resources/integration_azure_managed_app_client.json
 AZURE_PROPERTIES_OUTPUT_FILE_PATH="$(dirname $0)"/src/test/resources/integration_azure_env.properties
@@ -14,11 +14,6 @@ docker run --rm --cap-add IPC_LOCK \
             -e VAULT_TOKEN=$VAULT_TOKEN ${DSDE_TOOLBOX_DOCKER_IMAGE} \
             vault read -format json ${VAULT_AZURE_MANAGED_APP_CLIENT_PATH} \
             | jq -r .data > ${AZURE_MANAGED_APP_CLIENT_OUTPUT_FILE_PATH}
-
-# The docker image above doesn't have an image that will run on ARM architecture (such as the apple m1 chip)
-# The command below is an alternative with an installed version of vault
-# VAULT_ADDR="https://clotho.broadinstitute.org:8200"
-# vault read -format=json "${VAULT_AZURE_MANAGED_APP_CLIENT_PATH}" | jq -r .data > ${AZURE_MANAGED_APP_CLIENT_OUTPUT_FILE_PATH}
 
 AZURE_MANAGED_APP_CLIENT_ID=$(jq -r '."client-id"' ${AZURE_MANAGED_APP_CLIENT_OUTPUT_FILE_PATH})
 AZURE_MANAGED_APP_CLIENT_SECRET=$(jq -r '."client-secret"' ${AZURE_MANAGED_APP_CLIENT_OUTPUT_FILE_PATH})
