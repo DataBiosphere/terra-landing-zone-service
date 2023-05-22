40e181579ce4bc2b867a1e6da9dc69e57c6eb3a0
Blake Geno <bgeno@broadinstitute.org>
2023-05-22 10:39:11 -0400

    fix properties path

--- service/src/test/java/bio/terra/landingzone/stairway/flight/create/CreateLandingZoneResourcesFlightIntegrationTest.java
+++ service/src/test/java/bio/terra/landingzone/stairway/flight/create/CreateLandingZoneResourcesFlightIntegrationTest.java
@@ -33,6 +33,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -40,6 +41,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Tag("integration")
 @ActiveProfiles("test")
+@PropertySource("classpath:integration_azure_env.properties")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest()
 @SpringBootApplication(
