ee806f9c2c1df327cd561aff6fcb9fd8dcc68e35
Sergiy Getlin <91149690+sergiygetlin@users.noreply.github.com>
2023-05-30 15:39:10 -0700

    WOR-1002 Adjust existing delete mechanism to handle protected data landing zone (#129)
    * WOR-1002
    
    * Add test.
    
    * Uncomment code. Adjust config.
    
    * Add configuration for lts account ids.
    
    * Add configuration for uswest2 region. Switch test to useast2 region.
    
    * Resource id should be fully qualified.
    
    * Fix typo.
    
    * Extend code doc.
    
    ---------
    
    Co-authored-by: Sergiy Getlin <sergiygetlin@microsoft.com>

---
+++ service/src/test/java/bio/terra/landingzone/stairway/flight/create/LandingZoneRequestFixtures.java
@@ -0,0 +1,54 @@
+package bio.terra.landingzone.stairway.flight.create;
+
+import bio.terra.landingzone.db.model.LandingZoneRecord;
+import bio.terra.landingzone.service.landingzone.azure.model.LandingZoneRequest;
+import bio.terra.landingzone.stairway.flight.StepsDefinitionFactoryType;
+import bio.terra.profile.model.ProfileModel;
+import com.azure.core.management.profile.AzureProfile;
+import java.time.OffsetDateTime;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+public class LandingZoneRequestFixtures {
+  private LandingZoneRequestFixtures() {}
+
+  public static LandingZoneRequest createCromwellLZRequest(
+      UUID landingZoneId, UUID billingProfileId) {
+    return new LandingZoneRequest(
+        StepsDefinitionFactoryType.CROMWELL_BASE_DEFINITION_STEPS_PROVIDER_TYPE.getValue(),
+        "v1",
+        Map.of(),
+        billingProfileId,
+        Optional.of(landingZoneId));
+  }
+
+  public static LandingZoneRequest createProtectedDataLZRequest(
+      UUID landingZoneId, UUID billingProfileId) {
+    return new LandingZoneRequest(
+        StepsDefinitionFactoryType.PROTECTED_DATA_DEFINITION_STEPS_PROVIDER_NAME.getValue(),
+        "v1",
+        Map.of(),
+        billingProfileId,
+        Optional.of(landingZoneId));
+  }
+
+  public static LandingZoneRecord createLandingZoneRecord(
+      UUID landingZoneId,
+      String resourceGroupName,
+      AzureProfile azureProfile,
+      ProfileModel profile) {
+    return new LandingZoneRecord(
+        landingZoneId,
+        resourceGroupName,
+        "definition",
+        "version",
+        azureProfile.getSubscriptionId(),
+        azureProfile.getTenantId(),
+        profile.getId(),
+        OffsetDateTime.now(),
+        Optional.of("displayName"),
+        Optional.of("name"),
+        Map.of());
+  }
+}
