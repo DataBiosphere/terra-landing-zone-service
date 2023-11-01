3118b157b367254fcadecc8c822771b1c961cb32
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-11-01 16:48:18 -0700

    WOR-1117 Create separate gradle project in the landing zone repo to run as standalone service.

---
+++ futureservice/src/main/java/bio/terra/lz/futureservice/TestController.java
@@ -0,0 +1,12 @@
+package bio.terra.lz.futureservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+  @GetMapping("/")
+  public String index() {
+    return "Test";
+  }
+}
