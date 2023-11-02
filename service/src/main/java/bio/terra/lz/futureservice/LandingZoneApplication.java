68938bae91878f6ea2f3b7b5844e13c79509104a
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-11-02 10:32:20 -0700

    futureservice -> service

--- service/src/main/java/bio/terra/lz/futureservice/LandingZoneApplication.java
+++ service/src/main/java/bio/terra/lz/futureservice/LandingZoneApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// temporarily disable autoconfiguration since we don't have it yet.
+// temporarily disable autoconfiguration since we don't have any related db yet.
 // this prevents from running application
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LandingZoneApplication {
