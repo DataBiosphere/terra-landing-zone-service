532bb83e51c5d714c08ba7330964106cd621e323
Sergiy Getlin <sergiygetlin@microsoft.com>
2023-11-01 18:02:14 -0700

    Use application.yml. Link lz library.

--- futureservice/src/main/java/bio/terra/lz/futureservice/TestController.java
+++ futureservice/src/main/java/bio/terra/lz/futureservice/TestController.java
@@ -1,10 +1,14 @@
 package bio.terra.lz.futureservice;
 
+import bio.terra.landingzone.service.landingzone.azure.LandingZoneService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+  // no usage for now, but just make sure that lz library is linked
+  LandingZoneService lzService;
+
   @GetMapping("/")
   public String index() {
     return "Test";
