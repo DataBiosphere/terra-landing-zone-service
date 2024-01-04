923f0943391639188e60cb3dc9c48fa1e2e0eab6
Sergiy Getlin <sergiygetlin@microsoft.com>
2024-01-04 15:43:48 -0800

    Remove comment.

--- library/src/main/java/bio/terra/landingzone/library/landingzones/definition/FactoryDefinitionInfo.java
+++ library/src/main/java/bio/terra/landingzone/library/landingzones/definition/FactoryDefinitionInfo.java
@@ -2,6 +2,5 @@ package bio.terra.landingzone.library.landingzones.definition;
 
 import java.util.List;
 
-// TODO: do we need className?
 public record FactoryDefinitionInfo(
     String name, String description, String className, List<DefinitionVersion> versions) {}
