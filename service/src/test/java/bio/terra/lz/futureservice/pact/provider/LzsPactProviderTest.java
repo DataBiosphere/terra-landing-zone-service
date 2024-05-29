6c04fc54f01cdaefdbe39e54cc3fcfa8333b9657
aherbst-broad <aherbst@broadinstitute.org>
2024-05-29 11:31:17 -0400

    Rename pact-test => pact-consumer-test, provider-test => pact-provider-test

--- service/src/test/java/bio/terra/lz/futureservice/pact/provider/LzsPactProviderTest.java
+++ service/src/test/java/bio/terra/lz/futureservice/pact/provider/LzsPactProviderTest.java
@@ -52,7 +52,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
-@Tag("provider-test")
+@Tag("pact-provider-test")
 @Provider("terra-landing-zone-service")
 @PactBroker
 @SpringBootTest(
