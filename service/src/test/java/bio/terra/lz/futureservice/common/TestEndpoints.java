5652794b2369ada5af1c3355deded1d142d52034
Sergiy Getlin <91149690+sergiygetlin@users.noreply.github.com>
2024-06-21 08:45:07 -0700

    WOR-1663 [LZ Pentest] Endpoints should return caching directives. (#478)

---
+++ service/src/test/java/bio/terra/lz/futureservice/common/TestEndpoints.java
@@ -0,0 +1,11 @@
+package bio.terra.lz.futureservice.common;
+
+public class TestEndpoints {
+  private TestEndpoints() {}
+
+  public static final String AZURE_LANDING_ZONE_PATH = "/api/landingzones/v1/azure";
+  public static final String GET_CREATE_AZURE_LANDING_ZONE_RESULT =
+      "/api/landingzones/v1/azure/create-result";
+  public static final String LIST_AZURE_LANDING_ZONES_DEFINITIONS_PATH =
+      "/api/landingzones/definitions/v1/azure";
+}
